263365adc03a232200211e3ab2540aa103630fda
Nmb1171 <nahombadebo72@gmail.com>
2024-11-19 14:22:35 +0300

    More on user authenthication and template loading

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,11 +2,14 @@ from django.contrib.auth import views as auth_views
 from django.urls import path
 from .views import list_books, LibraryDetailView
 from .views import admin_view, librarian_view, member_view 
+from django.contrib.auth.views import LogoutView, LoginView
 from .views import register_view
+from . import views
 
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', register_view, name='register'),
     path('books/', list_books, name='list_books'),
