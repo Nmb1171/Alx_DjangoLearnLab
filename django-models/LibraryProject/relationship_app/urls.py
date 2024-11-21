4ed3dddf086ee9560a582bc8732d7bd23ab58114
Nmb1171 <nahombadebo72@gmail.com>
2024-11-21 16:50:43 +0300

    Using Django custom  permission

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,6 +2,7 @@ from django.contrib.auth import views as auth_views
 from django.urls import path
 from .views import list_books, LibraryDetailView
 from .views import admin_view, librarian_view, member_view 
+from .views import add_book, edit_book, delete_book
 from django.contrib.auth.views import LogoutView, LoginView
 from .views import register_view
 from . import views
@@ -19,4 +20,7 @@ urlpatterns = [
     path('admin_view/', admin_view, name='admin_view'),
     path('librarian_view/', librarian_view, name='librarian_view'),
     path('member_view/', member_view, name='member_view'),
+    path('add/', add_book, name='add_book'),
+    path('edit/<int:pk>/', edit_book, name='edit_book'),
+    path('delete/<int:pk>/', delete_book, name='delete_book')
 ]
