d7b2053a2b2301dfae536210304cfa970dae2d79
Nmb1171 <nahombadebo72@gmail.com>
2024-11-24 14:48:28 +0300

    Managing Permissions and Groups

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from .models import Article
 from django.contrib.auth.decorators import permission_required
 
 
 @permission_required('bookshelf.can_edit', raise_exception=True)
 def edit_article(request):
     pass 
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    """
+    View to display a list of books/articles.
+    Requires the user to have the 'can_view' permission.
+    """
+    books = Article.objects.all()  # Replace Article with your actual model name
+    return render(request, 'bookshelf/book_list.html', {'books': books})
 
 # Create your views here.
