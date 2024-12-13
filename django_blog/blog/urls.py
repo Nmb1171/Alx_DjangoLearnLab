bee647110f55c84bc7abc479139b4caa7502cefb
Nmb1171 <nahombadebo72@gmail.com>
2024-12-13 23:32:34 +0300

    Enhance the interactivity of Django blog project by implementing a comment system. This will allow users to leave comments on blog posts, fostering community engagement and discussion

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -17,6 +17,6 @@ urlpatterns = [
 
 
     path('posts/<int:post_id>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
-    path('comments/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='comment-update'),
-    path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
+    path('comment/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='comment-update'),
+    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
 ]
