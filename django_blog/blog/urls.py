78ff2ffec308a3e2ce07b52b8ad0bf9bac5260b4
Nmb1171 <nahombadebo72@gmail.com>
2024-12-13 22:56:19 +0300

    Develop and integrate features that allow users to create, read, update, and delete (CRUD) blog posts. This will enable authors to manage their content dynamically and viewers to browse blog entries effectively.

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,9 +3,15 @@ from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('', views.home, name='home'),
     path('login/', LoginView.as_view(template_name = 'blog/login.html'), name = 'login'),
     path('logout/', LogoutView.as_view(next_page = 'home'), name = 'logout'),
     path('register/', views.register, name='register'),
-    path('profile/', views.profile, name='profile')
+    path('profile/', views.profile, name='profile'),
+
+    #Blog-related URLs
+    path('', views.PostListView.as_view(), name='home'),  # List all posts
+    path('posts/new/', views.PostCreateView.as_view(), name='post-create'),  # Create a new post
+    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),  # View post details
+    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'),  # Edit a post
+    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),  # Delete a post
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
+from .models import Post
+from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 # Create your views here.
 def home(request):
     return render(request, 'blog/base.html')
@@ -44,3 +49,44 @@ def profile(request):
 
     return render(request, 'blog/profile.html')
 
+
+class PostListView(generic.ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-published_date']
+
+
+class PostDetailView(generic.DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+
+class PostCreateView(CreateView, LoginRequiredMixin):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+    
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('home')
+    template_name = 'blog/post_confirm_delete.html'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
