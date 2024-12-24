10edfa94de8306f5090601711816e523b105308d
Nmb1171 <nahombadebo72@gmail.com>
2024-12-24 20:50:55 +0300

    Minor

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -8,6 +8,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 # viewsets.ModelViewSet
+# Post.objects.filter(author__in=following_users).order_by", "permissions.IsAuthenticated
 # Create your views here.
 
 
