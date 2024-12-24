70bd9989b117d2429a80237b6687da95454b4fea
Nmb1171 <nahombadebo72@gmail.com>
2024-12-24 20:46:41 +0300

    Minor

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-
+# from rest_framework.authtoken.models import Token", "serializers.CharField()", "Token.objects.create", "get_user_model().objects.create_user
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField(required=True)
